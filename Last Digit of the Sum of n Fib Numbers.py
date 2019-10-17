d7c6c801fb39c2e7d6c376c4faafddf7834888f2
thed0ct0R-byte <55763303+thed0ct0R-byte@users.noreply.github.com>
2019-10-17 23:39:14 +0530

    Update Last Digit of the Sum of n Fib Numbers.py
    Code refactoring

--- Last Digit of the Sum of n Fib Numbers.py
+++ Last Digit of the Sum of n Fib Numbers.py
@@ -1,8 +1,8 @@
-def fibSum(a):
+def fib_sum(a):
     # Principle used : Sum of the first n Fibonacci numbers  = fib(n+2) - 1
-    a+=3
-    arr = [0]*(a)
-    arr[0],arr[1]=0,1
-    for i in range(2,a):
-        arr[i]=arr[i-1]+arr[i-2]
-    return (arr[-1]-1)%10
+    a += 3
+    arr = [0] * a
+    arr[0], arr[1] = 0, 1
+    for i in range(2, a):
+        arr[i] = arr[i - 1] + arr[i - 2]
+    return (arr[-1] - 1) % 10
