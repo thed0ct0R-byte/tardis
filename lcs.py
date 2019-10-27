49189d9e18fc0edf531b9a6f9111feeb398398b3
thed0ct0R-byte <55763303+thed0ct0R-byte@users.noreply.github.com>
2019-10-27 14:27:30 +0530

    Removed unwanted updates

--- lcs.py
+++ lcs.py
@@ -1,9 +1,5 @@
 def lcs(a, b):
     l = [[0 for _ in range(len(a) + 1)] for _ in range(len(b) + 1)]
-    for i in range(len(a) + 1):
-        l[0][i] = 0
-    for i in range(len(b) + 1):
-        l[i][0] = 0
     for i in range(1, len(b) + 1):
         for j in range(1, len(a) + 1):
             if a[j-1] == b[i-1]:
